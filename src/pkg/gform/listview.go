b0fb7424e5c2c98ab1577adbe5cdf9b61572ac37
Allen Dang <allengnr@gmail.com>
2012-01-10 14:21:13 +0800

    Rename all getter methods from "GetXXX" to "XXX".

--- src/pkg/gform/listview.go
+++ src/pkg/gform/listview.go
@@ -39,6 +39,14 @@ func (this *ListView) init(parent Controller) {
     RegMsgHandler(this)
 }
 
+// Changes the state of an item in a list-view control. Refer LVM_SETITEMSTATE message.
+func (this *ListView) setItemState(i int, state, mask uint) {
+    var item w32.LVITEM
+    item.State, item.StateMask = state, mask
+
+    user32.SendMessage(this.hwnd, w32.LVM_SETITEMSTATE, uintptr(i), uintptr(unsafe.Pointer(&item)))
+}
+
 func (this *ListView) EnableSingleSelect(enable bool) {
     ToggleStyle(this.hwnd, enable, w32.LVS_SINGLESEL)
 }
@@ -83,7 +91,7 @@ func (this *ListView) SetItemCount(count int) bool {
     return user32.SendMessage(this.hwnd, w32.LVM_SETITEMCOUNT, uintptr(count), 0) != 0
 }
 
-func (this *ListView) GetItemCount() int {
+func (this *ListView) ItemCount() int {
     return int(user32.SendMessage(this.hwnd, w32.LVM_GETITEMCOUNT, 0, 0))
 }
 
@@ -109,7 +117,7 @@ func (this *ListView) InsertItem(caption string, index int) {
 }
 
 func (this *ListView) AddItem(caption string) {
-    this.InsertItem(caption, this.GetItemCount())
+    this.InsertItem(caption, this.ItemCount())
 }
 
 func (this *ListView) InsertLvColumn(lvColumn *w32.LVCOLUMN, iCol int) {
@@ -124,22 +132,22 @@ func (this *ListView) DeleteAllItems() bool {
     return user32.SendMessage(this.hwnd, w32.LVM_DELETEALLITEMS, 0, 0) == w32.TRUE
 }
 
-func (this *ListView) GetItem(item *w32.LVITEM) bool {
+func (this *ListView) Item(item *w32.LVITEM) bool {
     return user32.SendMessage(this.hwnd, w32.LVM_GETITEM, 0, uintptr(unsafe.Pointer(item))) == w32.TRUE
 }
 
-func (this *ListView) GetItemAtIndex(i int) *w32.LVITEM {
+func (this *ListView) ItemAtIndex(i int) *w32.LVITEM {
     var item w32.LVITEM
     item.Mask = w32.LVIF_PARAM | w32.LVIF_TEXT
     item.IItem = i
 
-    this.GetItem(&item)
+    this.Item(&item)
     return &item
 }
 
 // mask is used to set the LVITEM.Mask for ListView.GetItem which indicates which attributes you'd like to receive
 // of LVITEM.
-func (this *ListView) GetSelectedItems(mask uint) []*w32.LVITEM {
+func (this *ListView) SelectedItems(mask uint) []*w32.LVITEM {
     items := make([]*w32.LVITEM, 0)
 
     var i int = -1
@@ -151,15 +159,20 @@ func (this *ListView) GetSelectedItems(mask uint) []*w32.LVITEM {
         var item w32.LVITEM
         item.Mask = mask
         item.IItem = i
-        if this.GetItem(&item) {
+        if this.Item(&item) {
             items = append(items, &item)
         }
     }
     return items
 }
 
-func (this *ListView) GetSelectedCount() uint {
+func (this *ListView) SelectedCount() uint {
     return uint(user32.SendMessage(this.hwnd, w32.LVM_GETSELECTEDCOUNT, 0, 0))
+}
+
+// Set i to -1 to select all items.
+func (this *ListView) SetSelectedItem(i int) {
+    this.setItemState(i, w32.LVIS_SELECTED, w32.LVIS_SELECTED)
 }
 
 func (this *ListView) SetImageList(imageList *ImageList, imageListType int) *ImageList {
@@ -171,7 +184,7 @@ func (this *ListView) SetImageList(imageList *ImageList, imageListType int) *Ima
     return &ImageList{w32.HIMAGELIST(h)}
 }
 
-func (this *ListView) GetImageList(imageListType int) *ImageList {
+func (this *ListView) ImageList(imageListType int) *ImageList {
     h := user32.SendMessage(this.hwnd, w32.LVM_GETIMAGELIST, uintptr(imageListType), 0)
     if h == 0 {
         return nil
