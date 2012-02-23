4831c192c44d9e5a152371015feab8bfa75d388b
Allen Dang <allengnr@gmail.com>
2012-02-23 12:25:48 +0800

    use GOPATH instead of hardcode path.

--- startkit/make.bat
+++ startkit/make.bat
@@ -1,5 +1,6 @@
+@echo off
 windres resource.rc -o temp-rc.o 
-go tool 8g -I c:\Users\e489093\Code\Go\pkg\windows_386 app.go
+go tool 8g -I %GOPATH%\pkg\windows_386 app.go
 go tool pack grc _go_.8 app.8 temp-rc.o
-go tool 8l -L c:\Users\e489093\Code\Go\pkg\windows_386 -o startkit.exe -s -Hwindowsgui _go_.8
+go tool 8l -L %GOPATH%\pkg\windows_386 -o startkit.exe -s -Hwindowsgui _go_.8
 del /F /Q *.8 *.o
